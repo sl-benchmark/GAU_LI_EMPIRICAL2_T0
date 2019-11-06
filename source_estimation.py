bf81e5b16201cb5a22c185b08f1f2545285623be
stanislas jouven <stanislasjouven@gmail.com>
2019-11-06 23:29:35 +0100

    cast error

--- source_estimation.py
+++ source_estimation.py
@@ -64,7 +64,7 @@ def ml_estimate(graph, obs_time, path_lengths, max_dist=np.inf):
             print(path_lengths)
             print(path_lengths.transpose())
             ### Covariance matrix
-            cov_d_s = np.cov(path_lengths.transpose()[str(int(s))].values - mean_path_lengths[str(sorted_obs[0])][s])
+            cov_d_s = np.cov(path_lengths.transpose()[str(s)].values - mean_path_lengths[str(sorted_obs[0])][s])
             ### Mean vector
             mu_s = tl.mu_vector_s(mean_path_lengths, s, sorted_obs)
             ### Computes log-probability of the source being the real source
