e61cc3371d0b63ca68dcd540b25ffa0655d0a90f
stanislas jouven <stanislasjouven@gmail.com>
2019-11-06 23:24:11 +0100

    print

--- source_estimation.py
+++ source_estimation.py
@@ -61,6 +61,7 @@ def ml_estimate(graph, obs_time, path_lengths, max_dist=np.inf):
         tmp_lkl = [] # Used to compute mean of likelihoods of same class
         for s in c:
             print('""""""""""')
+            print(path_lengths)
             print(path_lengths.transpose())
             ### Covariance matrix
             cov_d_s = np.cov(path_lengths.transpose()[str(int(s))].values - mean_path_lengths[str(sorted_obs[0])][s])
