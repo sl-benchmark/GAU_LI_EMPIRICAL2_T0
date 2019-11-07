aa67a98525b30cb48c264ed4b1774d924838c9f4
stanislas jouven <stanislasjouven@gmail.com>
2019-11-07 11:28:09 +0100

    print

--- source_estimation.py
+++ source_estimation.py
@@ -74,6 +74,7 @@ def ml_estimate(graph, obs_time, path_lengths, max_dist=np.inf):
             print('SHAPE ', cov_d_s.shape)
             ### Mean vector
             mu_s = tl.mu_vector_s(mean_path_lengths, s, sorted_obs)
+            print('SHAPE mean', mu_s.shape)
             ### Computes log-probability of the source being the real source
             likelihood, tmp = logLH_source_tree(mu_s, cov_d_s, sorted_obs, obs_time)
             tmp_lkl.append(likelihood)
