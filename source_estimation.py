d5e04ad2b4a8badffb573b26828ff9f1e02c112c
stanislas jouven <stanislasjouven@gmail.com>
2019-11-07 09:18:53 +0100

    print

--- source_estimation.py
+++ source_estimation.py
@@ -68,9 +68,8 @@ def ml_estimate(graph, obs_time, path_lengths, max_dist=np.inf):
             ### Covariance matrix
             print('S ', s)
             print('hahahahha')
-            print(path_lengths.transpose().reset_index())
-            print(mean_path_lengths)
             cov_d_s = np.cov(path_lengths.transpose().reset_index()[s].to_numpy() - mean_path_lengths[str(sorted_obs[0])][s])
+            print(path_lengths.transpose().reset_index()[s])
             ### Mean vector
             mu_s = tl.mu_vector_s(mean_path_lengths, s, sorted_obs)
             ### Computes log-probability of the source being the real source
