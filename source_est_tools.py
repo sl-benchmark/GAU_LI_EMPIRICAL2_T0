4f4e3ff93a7efd4c463b9a76335de79b0f459bd4
stanislas jouven <stanislasjouven@gmail.com>
2019-11-17 12:34:58 +0100

    syntax error

--- source_est_tools.py
+++ source_est_tools.py
@@ -53,7 +53,7 @@ def mu_vector_s(path_lengths, s, obs):
 def cov_matrix(path_lengths, sorted_obs, s):
     ref_obs = sorted_obs[0]
     ref_time = path_lengths[str(ref_obs)].loc[s]
-    ref_time = np.tile(ref_time, (len(sorted_obs -1, 1)))
+    ref_time = np.tile(ref_time, (len(sorted_obs)-1, 1)))
 
     cov_d_s = np.cov(path_lengths.transpose().drop([str(ref_obs)]).reset_index()[s].to_numpy() - ref_time, ddof = 0)
 
