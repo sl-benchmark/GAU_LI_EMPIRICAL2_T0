dc8cfb488891a02506f035be0a67f36fab78341e
stanislas jouven <stanislasjouven@gmail.com>
2020-02-27 21:50:17 +0100

    print

--- source_estimation.py
+++ source_estimation.py
@@ -107,8 +107,8 @@ def logLH_source_tree(mu_s, cov_d, obs, obs_time, ref_obs):
     ### Loops over all the observers (w/o first one (referential) and last one (computation constraint))
     #   Every time it computes the infection time with respect to the ref obs
     print('len(obs)', len(obs))
-    for l in range(1, len(obs)):
-        obs_d[l-1] = obs_time[obs[l]] - obs_time[ref_obs]
+    for l in range(0, len(obs)):
+        obs_d[l] = obs_time[obs[l]] - obs_time[ref_obs]
     print('obs_d', len(obs_d))
     print('mu_s', len(mu_s))
 
