2f359e3880f6ca47dabc29068f9d80a7f8ce1328
stanislas jouven <stanislasjouven@gmail.com>
2020-02-27 10:27:20 +0100

    indices were not computed correctly

--- source_est_tools.py
+++ source_est_tools.py
@@ -38,8 +38,8 @@ def mu_vector_s(path_lengths, s, obs, ref_obs):
         obs = obs[1:]
     else:
         mu_s = np.zeros((K_0, 1))
-        indices = np.array(sorted(range(len(v)), key = lambda sub: v[sub])[-K_0:])
-        obs = np.array(obs)[indices+1][:K_0]
+        indices = np.array(sorted(range(len(v)), key = lambda sub: v[sub])[:K_0])
+        obs = np.array(obs)[indices+1]
         v = sorted(v)
         v = v[:K_0]
     mu_s[:, 0] = v
