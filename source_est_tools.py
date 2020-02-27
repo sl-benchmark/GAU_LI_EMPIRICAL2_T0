8fa7c9b5e3ca9f6fc24c123177c057df3f466134
stanislas jouven <stanislasjouven@gmail.com>
2020-02-27 10:07:30 +0100

    convert the observer list into np array

--- source_est_tools.py
+++ source_est_tools.py
@@ -38,9 +38,8 @@ def mu_vector_s(path_lengths, s, obs, ref_obs):
         obs = obs[1:]
     else:
         mu_s = np.zeros((K_0, 1))
-        indices = np.array(sorted(range(len(v)), key = lambda sub: v[sub])[-K_0:])
-        obs = obs[indices]
-        obs = obs[:K_0]
+        indices = sorted(range(len(v)), key = lambda sub: v[sub])[-K_0:]
+        obs = np.array(obs)[indices][:K_0]
         v = sorted(v)
         v = v[:K_0]
     mu_s[:, 0] = v
