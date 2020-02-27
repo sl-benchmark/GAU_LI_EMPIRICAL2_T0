9c5b8b6c8c8868ab15d4df5dccd1120023dc45a7
stanislas jouven <stanislasjouven@gmail.com>
2020-02-27 10:16:51 +0100

    convert indices into np array

--- source_est_tools.py
+++ source_est_tools.py
@@ -38,7 +38,7 @@ def mu_vector_s(path_lengths, s, obs, ref_obs):
         obs = obs[1:]
     else:
         mu_s = np.zeros((K_0, 1))
-        indices = sorted(range(len(v)), key = lambda sub: v[sub])[-K_0:]
+        indices = np.array(sorted(range(len(v)), key = lambda sub: v[sub])[-K_0:])
         obs = np.array(obs)[indices+1][:K_0]
         v = sorted(v)
         v = v[:K_0]
